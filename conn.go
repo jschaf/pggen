ab38e2ea4fd82f7a897b48c26e6d4b4da07635c9
Joe Schafer <joe@jschaf.com>
2021-01-18 16:32:51 -0800

    Move Conn to sqld top level package

---
+++ conn.go
@@ -0,0 +1,26 @@
+package sqld
+
+import (
+	"context"
+	"github.com/jackc/pgconn"
+	"github.com/jackc/pgx/v4"
+)
+
+// Conn is a connection to a Postgres database. This is usually backed by
+// *pgx.Conn, pgx.Tx, or *pgxpool.Pool.
+type Conn interface {
+	// Query executes sql with args. If there is an error the returned Rows will
+	// be returned in an error state. So it is allowed to ignore the error
+	// returned from Query and handle it in Rows.
+	Query(ctx context.Context, sql string, args ...interface{}) (pgx.Rows, error)
+
+	// QueryRow is a convenience wrapper over Query. Any error that occurs while
+	// querying is deferred until calling Scan on the returned Row. That Row will
+	// error with pgx.ErrNoRows if no rows are returned.
+	QueryRow(ctx context.Context, sql string, args ...interface{}) pgx.Row
+
+	// Exec executes sql. sql can be either a prepared statement name or an SQL
+	// string. arguments should be referenced positionally from the sql string
+	// as $1, $2, etc.
+	Exec(ctx context.Context, sql string, arguments ...interface{}) (pgconn.CommandTag, error)
+}
--- example/author/querier.go
+++ example/author/querier.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/jackc/pgconn"
 	"github.com/jackc/pgx/v4"
+	"github.com/jschaf/sqld"
 )
 
 // Querier is a typesafe Go interface backed by SQL queries.
@@ -29,34 +30,15 @@ type Querier interface {
 	DeleteAuthorsScan(ctx context.Context, results pgx.BatchResults) (pgconn.CommandTag, error)
 }
 
-// Conn is a connection to a Postgres database. This is usually backed by
-// *pgx.Conn, pgx.Tx, or *pgxpool.Pool.
-type Conn interface {
-	// Query executes sql with args. If there is an error the returned Rows will
-	// be returned in an error state. So it is allowed to ignore the error
-	// returned from Query and handle it in Rows.
-	Query(ctx context.Context, sql string, args ...interface{}) (pgx.Rows, error)
-
-	// QueryRow is a convenience wrapper over Query. Any error that occurs while
-	// querying is deferred until calling Scan on the returned Row. That Row will
-	// error with pgx.ErrNoRows if no rows are returned.
-	QueryRow(ctx context.Context, sql string, args ...interface{}) pgx.Row
-
-	// Exec executes sql. sql can be either a prepared statement name or an SQL
-	// string. arguments should be referenced positionally from the sql string
-	// as $1, $2, etc.
-	Exec(ctx context.Context, sql string, arguments ...interface{}) (pgconn.CommandTag, error)
-}
-
 type DBQuerier struct {
-	conn  Conn
+	conn  sqld.Conn
 	hooks QueryHook
 }
 
 var _ Querier = &DBQuerier{}
 
 // NewQuerier creates a DBQuerier that implements Querier.
-func NewQuerier(conn Conn, hooks QueryHook) *DBQuerier {
+func NewQuerier(conn sqld.Conn, hooks QueryHook) *DBQuerier {
 	return &DBQuerier{
 		conn:  conn,
 		hooks: hooks,
