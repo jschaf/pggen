5d439fff95b6811f38ca519d1c9c5a2ec192cd1e
Joe Schafer <joe@jschaf.com>
2021-01-20 15:48:47 -0800

    Make test check for same contents in generated query file

--- codegen/gen_test.go
+++ codegen/gen_test.go
@@ -3,6 +3,7 @@ package codegen
 import (
 	"github.com/jschaf/sqld/pgtest"
 	"github.com/stretchr/testify/assert"
+	"io/ioutil"
 	"path/filepath"
 	"testing"
 )
@@ -27,6 +28,19 @@ func TestGenerate_Example_Author(t *testing.T) {
 		t.Fatalf("Generate() example/author: %s", err)
 	}
 
-	assert.FileExists(t, filepath.Join(tmpDir, "queries.sql.go"),
+	wantQueriesFile := "../example/author/queries.sql.go"
+	gotQueriesFile := filepath.Join(tmpDir, "queries.sql.go")
+	assert.FileExists(t, gotQueriesFile,
 		"Generate() should emit queries.sql.go")
+	wantQueries, err := ioutil.ReadFile(wantQueriesFile)
+	if err != nil {
+		t.Fatalf("read wanted queries.go.sql: %s", err)
+	}
+	gotQueries, err := ioutil.ReadFile(gotQueriesFile)
+	if err != nil {
+		t.Fatalf("read generated queries.go.sql: %s", err)
+	}
+	assert.Equalf(t, string(wantQueries), string(gotQueries),
+		"Got file %s; does not match contents of %s",
+		gotQueriesFile, wantQueriesFile)
 }
