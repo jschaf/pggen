6e592a8abf79d1d5426687f61f82bb12f8719860
Joe Schafer <joe@jschaf.com>
2021-01-24 15:13:08 -0800

    Add casing package for custom casing
    The strcase package doesn't support acronyms delimited by underscore.

---
+++ internal/casing/casing.go
@@ -0,0 +1,66 @@
+package casing
+
+import "strings"
+
+type Caser struct {
+	acronyms map[string]string
+}
+
+func NewCaser() Caser {
+	return Caser{
+		acronyms: map[string]string{},
+	}
+}
+
+func (cs Caser) AddAcronyms(acros map[string]string) {
+	for a, b := range acros {
+		cs.acronyms[a] = b
+	}
+}
+
+func isLower(ch byte) bool { return 'a' <= ch && ch <= 'z' }
+func isUpper(ch byte) bool { return 'A' <= ch && ch <= 'Z' }
+
+func upper(ch byte) byte { return ch - ('a' - 'A') } // returns upper-case ch iff ch is ASCII letter
+
+func (cs Caser) appendUpperCamel(sb *strings.Builder, chars []byte, lo, hi int) {
+	if lo == hi {
+		return
+	}
+	wordChars := chars[lo:hi]
+	word := string(wordChars)
+	if a, ok := cs.acronyms[word]; ok {
+		sb.WriteString(a)
+		return
+	}
+	if isLower(chars[lo]) {
+		sb.WriteByte(upper(chars[lo]))
+	} else {
+		sb.WriteByte(chars[lo])
+	}
+	for i := lo + 1; i < hi; i++ {
+		sb.WriteByte(chars[i])
+	}
+}
+
+func (cs Caser) ToUpperCamel(s string) string {
+	s = strings.TrimSpace(s)
+	sb := &strings.Builder{}
+	sb.Grow(len(s))
+	// Find underscore delimited word.
+	chars := []byte(s)
+	lo, hi := 0, 0
+	for i, ch := range chars {
+		hi = i
+		switch {
+		case ch == '_':
+			cs.appendUpperCamel(sb, chars, lo, hi)
+			lo = i + 1 // skip underscore
+		case isUpper(ch):
+			cs.appendUpperCamel(sb, chars, lo, hi)
+			lo = i
+		}
+	}
+	cs.appendUpperCamel(sb, chars, lo, hi+1)
+	return sb.String()
+}
---
+++ internal/casing/casing_test.go
@@ -0,0 +1,37 @@
+package casing
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestToUpperCamel(t *testing.T) {
+	tests := []struct {
+		word     string
+		want     string
+		acronyms map[string]string
+	}{
+		{"fooBar", "FooBar", nil},
+		{"foo", "Foo", nil},
+		{"foo123", "Foo123", nil},
+		{"foo123bar", "Foo123bar", nil},
+		{"foo123bar_baz", "Foo123barBaz", nil},
+		{"foo", "FOO", map[string]string{"foo": "FOO"}},
+		{"foo_", "Foo", nil},
+		{"_foo_", "Foo", nil},
+		{"foo__", "Foo", nil},
+		{"foo__bar", "FooBar", nil},
+		{"foo_bar", "FooBar", nil},
+		{"foo_bar_baz", "FooBarBaz", nil},
+		{"foo_bar_baz", "FooBarBAZ", map[string]string{"baz": "BAZ"}},
+		{"foo_bar_baz", "FooBARBAZ", map[string]string{"bar": "BAR", "baz": "BAZ"}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.word+"="+tt.want, func(t *testing.T) {
+			caser := NewCaser()
+			caser.AddAcronyms(tt.acronyms)
+			got := caser.ToUpperCamel(tt.word)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
