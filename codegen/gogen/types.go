72529ecfc09377cfd8f1cedc36ea3936504bce75
Joe Schafer <joe@jschaf.com>
2021-01-25 12:18:25 -0800

    Add support for all types in pgtypes

--- codegen/gogen/types.go
+++ codegen/gogen/types.go
@@ -1,21 +1,81 @@
 package gogen
 
-import "github.com/jschaf/pggen/internal/pg"
+import (
+	"github.com/jackc/pgtype"
+	"github.com/jschaf/pggen/internal/pg"
+)
+
+var goPgTypes = map[pg.OIDInt]string{
+	pgtype.BoolOID:             "pgtype.Bool",
+	pgtype.QCharOID:            "pgtype.QChar",
+	pgtype.NameOID:             "pgtype.Name",
+	pgtype.Int8OID:             "pgtype.Int8",
+	pgtype.Int2OID:             "pgtype.Int2",
+	pgtype.Int4OID:             "pgtype.Int4",
+	pgtype.TextOID:             "pgtype.Text",
+	pgtype.OIDOID:              "pgtype.OID",
+	pgtype.TIDOID:              "pgtype.TID",
+	pgtype.XIDOID:              "pgtype.XID",
+	pgtype.CIDOID:              "pgtype.CID",
+	pgtype.JSONOID:             "pgtype.JSON",
+	pgtype.PointOID:            "pgtype.Point",
+	pgtype.LsegOID:             "pgtype.Lseg",
+	pgtype.PathOID:             "pgtype.Path",
+	pgtype.BoxOID:              "pgtype.Box",
+	pgtype.PolygonOID:          "pgtype.Polygon",
+	pgtype.LineOID:             "pgtype.Line",
+	pgtype.CIDROID:             "pgtype.CIDR",
+	pgtype.CIDRArrayOID:        "pgtype.CIDRArray",
+	pgtype.Float4OID:           "pgtype.Float4",
+	pgtype.Float8OID:           "pgtype.Float8",
+	pgtype.UnknownOID:          "pgtype.Unknown",
+	pgtype.CircleOID:           "pgtype.Circle",
+	pgtype.MacaddrOID:          "pgtype.Macaddr",
+	pgtype.InetOID:             "pgtype.Inet",
+	pgtype.BoolArrayOID:        "pgtype.BoolArray",
+	pgtype.ByteaArrayOID:       "pgtype.ByteaArray",
+	pgtype.Int2ArrayOID:        "pgtype.Int2Array",
+	pgtype.Int4ArrayOID:        "pgtype.Int4Array",
+	pgtype.TextArrayOID:        "pgtype.TextArray",
+	pgtype.BPCharArrayOID:      "pgtype.BPCharArray",
+	pgtype.VarcharArrayOID:     "pgtype.VarcharArray",
+	pgtype.Int8ArrayOID:        "pgtype.Int8Array",
+	pgtype.Float4ArrayOID:      "pgtype.Float4Array",
+	pgtype.Float8ArrayOID:      "pgtype.Float8Array",
+	pgtype.ACLItemOID:          "pgtype.ACLItem",
+	pgtype.ACLItemArrayOID:     "pgtype.ACLItemArray",
+	pgtype.InetArrayOID:        "pgtype.InetArray",
+	pgtype.BPCharOID:           "pgtype.BPChar",
+	pgtype.VarcharOID:          "pgtype.Varchar",
+	pgtype.DateOID:             "pgtype.Date",
+	pgtype.TimeOID:             "pgtype.Time",
+	pgtype.TimestampOID:        "pgtype.Timestamp",
+	pgtype.TimestampArrayOID:   "pgtype.TimestampArray",
+	pgtype.DateArrayOID:        "pgtype.DateArray",
+	pgtype.TimestamptzOID:      "pgtype.Timestamptz",
+	pgtype.TimestamptzArrayOID: "pgtype.TimestamptzArray",
+	pgtype.IntervalOID:         "pgtype.Interval",
+	pgtype.NumericArrayOID:     "pgtype.NumericArray",
+	pgtype.BitOID:              "pgtype.Bit",
+	pgtype.VarbitOID:           "pgtype.Varbit",
+	pgtype.NumericOID:          "pgtype.Numeric",
+	pgtype.RecordOID:           "pgtype.Record",
+	pgtype.UUIDOID:             "pgtype.UUID",
+	pgtype.UUIDArrayOID:        "pgtype.UUIDArray",
+	pgtype.JSONBOID:            "pgtype.JSONB",
+	pgtype.JSONBArrayOID:       "pgtype.JSONBArray",
+	pgtype.Int4rangeOID:        "pgtype.Int4range",
+	pgtype.NumrangeOID:         "pgtype.Numrange",
+	pgtype.TsrangeOID:          "pgtype.Tsrange",
+	pgtype.TstzrangeOID:        "pgtype.Tstzrange",
+	pgtype.DaterangeOID:        "pgtype.Daterange",
+	pgtype.Int8rangeOID:        "pgtype.Int8range",
+}
 
 // pgToGoType maps a Postgres type to a Go type.
 func pgToGoType(pgType pg.Type) string {
-	switch pgType.String() {
-	case "bool":
-		return "bool"
-	case "bytea":
-		return "[]byte"
-	case "text":
-		return "string"
-	case "integer", "int4":
-		return "int32"
-	case "int8", "bigint":
-		return "int64"
-	default:
-		return pgType.String()
+	if goType, ok := goPgTypes[pgType.OID]; ok {
+		return goType
 	}
+	return pgType.Name
 }
--- internal/pg/types.go
+++ internal/pg/types.go
@@ -7,12 +7,14 @@ import (
 	"sync"
 )
 
-// TypeKind is b for a base type, c for a composite type (e.g., a table's row type), d for a domain, e for an enum type,
-// p for a pseudo-type, or r for a range type. See also typrelid and typbasetype.
+type OIDInt = uint32
+
+// TypeKind is b for a base type, c for a composite type (e.g., a table's row
+// type), d for a domain, e for an enum type, p for a pseudo-type, or r for a
+// range type. See also typrelid and typbasetype.
 type TypeKind byte
 
-type OID = uint32
-
+//goland:noinspection GoUnusedConst
 const (
 	KindBaseType      TypeKind = 'b'
 	KindCompositeType TypeKind = 'c' // table row type
@@ -22,16 +24,15 @@ const (
 	KindRangeType     TypeKind = 'r'
 )
 
-type Type interface {
-	fmt.Stringer
-	OID() OID
-	pgType()
+type Type struct {
+	OID  OIDInt // pg_type.oid: row identifier
+	Name string // pg_type.typname: data type name
 }
 
 type (
 	// https://www.postgresql.org/docs/13/catalog-pg-type.html
 	BaseType struct {
-		ID         OID            // pg_type.oid: row identifier
+		ID         OIDInt         // pg_type.oid: row identifier
 		Name       string         // pg_type.typname: data type name
 		Kind       TypeKind       // pg_type.typtype: the kind of type
 		Composite  *CompositeType // pg_type.typrelid: composite type only, the pg_class for the type
@@ -39,7 +40,7 @@ type (
 	}
 
 	DomainType struct {
-		ID         OID      // pg_type.oid: row identifier
+		ID         OIDInt   // pg_type.oid: row identifier
 		Name       string   // pg_type.typname: data type name
 		IsNotNull  bool     // pg_type.typnotnull: domains only, not null constraint for domains
 		HasDefault bool     // pg_type.typdefault: domains only, if there's a default value
@@ -55,32 +56,149 @@ type (
 	}
 )
 
-func (b BaseType) String() string { return b.Name }
-func (b BaseType) OID() OID       { return b.ID }
-func (b BaseType) pgType()        {}
-
-func (c CompositeType) OID() OID { return c.ID }
-func (c CompositeType) pgType()  {}
-
+//goland:noinspection GoUnusedGlobalVariable
 var (
-	Bool = BaseType{ID: pgtype.BoolOID, Name: "bool", Kind: KindBaseType}
-	Text = BaseType{ID: pgtype.TextOID, Name: "text", Kind: KindBaseType}
-	Int4 = BaseType{ID: pgtype.Int4OID, Name: "integer", Kind: KindBaseType}
+	Bool             = Type{OID: pgtype.BoolOID, Name: "bool"}
+	Bytea            = Type{OID: pgtype.ByteaOID, Name: "bytea"}
+	QChar            = Type{OID: pgtype.QCharOID, Name: "char"}
+	Name             = Type{OID: pgtype.NameOID, Name: "name"}
+	Int8             = Type{OID: pgtype.Int8OID, Name: "int8"}
+	Int2             = Type{OID: pgtype.Int2OID, Name: "int2"}
+	Int4             = Type{OID: pgtype.Int4OID, Name: "int4"}
+	Text             = Type{OID: pgtype.TextOID, Name: "text"}
+	OID              = Type{OID: pgtype.OIDOID, Name: "oid"}
+	TID              = Type{OID: pgtype.TIDOID, Name: "tid"}
+	XID              = Type{OID: pgtype.XIDOID, Name: "xid"}
+	CID              = Type{OID: pgtype.CIDOID, Name: "cid"}
+	JSON             = Type{OID: pgtype.JSONOID, Name: "json"}
+	Point            = Type{OID: pgtype.PointOID, Name: "point"}
+	Lseg             = Type{OID: pgtype.LsegOID, Name: "lseg"}
+	Path             = Type{OID: pgtype.PathOID, Name: "path"}
+	Box              = Type{OID: pgtype.BoxOID, Name: "box"}
+	Polygon          = Type{OID: pgtype.PolygonOID, Name: "polygon"}
+	Line             = Type{OID: pgtype.LineOID, Name: "line"}
+	CIDR             = Type{OID: pgtype.CIDROID, Name: "cidr"}
+	CIDRArray        = Type{OID: pgtype.CIDRArrayOID, Name: "_cidr"}
+	Float4           = Type{OID: pgtype.Float4OID, Name: "float4"}
+	Float8           = Type{OID: pgtype.Float8OID, Name: "float8"}
+	Unknown          = Type{OID: pgtype.UnknownOID, Name: "unknown"}
+	Circle           = Type{OID: pgtype.CircleOID, Name: "circle"}
+	Macaddr          = Type{OID: pgtype.MacaddrOID, Name: "macaddr"}
+	Inet             = Type{OID: pgtype.InetOID, Name: "inet"}
+	BoolArray        = Type{OID: pgtype.BoolArrayOID, Name: "_bool"}
+	ByteaArray       = Type{OID: pgtype.ByteaArrayOID, Name: "_bytea"}
+	Int2Array        = Type{OID: pgtype.Int2ArrayOID, Name: "_int2"}
+	Int4Array        = Type{OID: pgtype.Int4ArrayOID, Name: "_int4"}
+	TextArray        = Type{OID: pgtype.TextArrayOID, Name: "_text"}
+	BPCharArray      = Type{OID: pgtype.BPCharArrayOID, Name: "_bpchar"}
+	VarcharArray     = Type{OID: pgtype.VarcharArrayOID, Name: "_varchar"}
+	Int8Array        = Type{OID: pgtype.Int8ArrayOID, Name: "_int8"}
+	Float4Array      = Type{OID: pgtype.Float4ArrayOID, Name: "_float4"}
+	Float8Array      = Type{OID: pgtype.Float8ArrayOID, Name: "_float8"}
+	ACLItem          = Type{OID: pgtype.ACLItemOID, Name: "aclitem"}
+	ACLItemArray     = Type{OID: pgtype.ACLItemArrayOID, Name: "_aclitem"}
+	InetArray        = Type{OID: pgtype.InetArrayOID, Name: "_inet"}
+	BPChar           = Type{OID: pgtype.BPCharOID, Name: "bpchar"}
+	Varchar          = Type{OID: pgtype.VarcharOID, Name: "varchar"}
+	Date             = Type{OID: pgtype.DateOID, Name: "date"}
+	Time             = Type{OID: pgtype.TimeOID, Name: "time"}
+	Timestamp        = Type{OID: pgtype.TimestampOID, Name: "timestamp"}
+	TimestampArray   = Type{OID: pgtype.TimestampArrayOID, Name: "_timestamp"}
+	DateArray        = Type{OID: pgtype.DateArrayOID, Name: "_date"}
+	Timestamptz      = Type{OID: pgtype.TimestamptzOID, Name: "timestamptz"}
+	TimestamptzArray = Type{OID: pgtype.TimestamptzArrayOID, Name: "_timestamptz"}
+	Interval         = Type{OID: pgtype.IntervalOID, Name: "interval"}
+	NumericArray     = Type{OID: pgtype.NumericArrayOID, Name: "_numeric"}
+	Bit              = Type{OID: pgtype.BitOID, Name: "bit"}
+	Varbit           = Type{OID: pgtype.VarbitOID, Name: "varbit"}
+	Numeric          = Type{OID: pgtype.NumericOID, Name: "numeric"}
+	Record           = Type{OID: pgtype.RecordOID, Name: "record"}
+	UUID             = Type{OID: pgtype.UUIDOID, Name: "uuid"}
+	UUIDArray        = Type{OID: pgtype.UUIDArrayOID, Name: "_uuid"}
+	JSONB            = Type{OID: pgtype.JSONBOID, Name: "jsonb"}
+	JSONBArray       = Type{OID: pgtype.JSONBArrayOID, Name: "_jsonb"}
+	Int4range        = Type{OID: pgtype.Int4rangeOID, Name: "int4range"}
+	Numrange         = Type{OID: pgtype.NumrangeOID, Name: "numrange"}
+	Tsrange          = Type{OID: pgtype.TsrangeOID, Name: "tsrange"}
+	Tstzrange        = Type{OID: pgtype.TstzrangeOID, Name: "tstzrange"}
+	Daterange        = Type{OID: pgtype.DaterangeOID, Name: "daterange"}
+	Int8range        = Type{OID: pgtype.Int8rangeOID, Name: "int8range"}
 )
 
 var (
 	typeMapLock = &sync.Mutex{}
 
-	typeMap = map[OID]Type{
-		pgtype.BoolOID: Bool,
-		pgtype.TextOID: Text,
-		pgtype.Int4OID: Int4,
+	typeMap = map[OIDInt]Type{
+		pgtype.BoolOID:             Bool,
+		pgtype.QCharOID:            QChar,
+		pgtype.NameOID:             Name,
+		pgtype.Int8OID:             Int8,
+		pgtype.Int2OID:             Int2,
+		pgtype.Int4OID:             Int4,
+		pgtype.TextOID:             Text,
+		pgtype.OIDOID:              OID,
+		pgtype.TIDOID:              TID,
+		pgtype.XIDOID:              XID,
+		pgtype.CIDOID:              CID,
+		pgtype.JSONOID:             JSON,
+		pgtype.PointOID:            Point,
+		pgtype.LsegOID:             Lseg,
+		pgtype.PathOID:             Path,
+		pgtype.BoxOID:              Box,
+		pgtype.PolygonOID:          Polygon,
+		pgtype.LineOID:             Line,
+		pgtype.CIDROID:             CIDR,
+		pgtype.CIDRArrayOID:        CIDRArray,
+		pgtype.Float4OID:           Float4,
+		pgtype.Float8OID:           Float8,
+		pgtype.UnknownOID:          Unknown,
+		pgtype.CircleOID:           Circle,
+		pgtype.MacaddrOID:          Macaddr,
+		pgtype.InetOID:             Inet,
+		pgtype.BoolArrayOID:        BoolArray,
+		pgtype.ByteaArrayOID:       ByteaArray,
+		pgtype.Int2ArrayOID:        Int2Array,
+		pgtype.Int4ArrayOID:        Int4Array,
+		pgtype.TextArrayOID:        TextArray,
+		pgtype.BPCharArrayOID:      BPCharArray,
+		pgtype.VarcharArrayOID:     VarcharArray,
+		pgtype.Int8ArrayOID:        Int8Array,
+		pgtype.Float4ArrayOID:      Float4Array,
+		pgtype.Float8ArrayOID:      Float8Array,
+		pgtype.ACLItemOID:          ACLItem,
+		pgtype.ACLItemArrayOID:     ACLItemArray,
+		pgtype.InetArrayOID:        InetArray,
+		pgtype.BPCharOID:           BPChar,
+		pgtype.VarcharOID:          Varchar,
+		pgtype.DateOID:             Date,
+		pgtype.TimeOID:             Time,
+		pgtype.TimestampOID:        Timestamp,
+		pgtype.TimestampArrayOID:   TimestampArray,
+		pgtype.DateArrayOID:        DateArray,
+		pgtype.TimestamptzOID:      Timestamptz,
+		pgtype.TimestamptzArrayOID: TimestamptzArray,
+		pgtype.IntervalOID:         Interval,
+		pgtype.NumericArrayOID:     NumericArray,
+		pgtype.BitOID:              Bit,
+		pgtype.VarbitOID:           Varbit,
+		pgtype.NumericOID:          Numeric,
+		pgtype.RecordOID:           Record,
+		pgtype.UUIDOID:             UUID,
+		pgtype.UUIDArrayOID:        UUIDArray,
+		pgtype.JSONBOID:            JSONB,
+		pgtype.JSONBArrayOID:       JSONBArray,
+		pgtype.Int4rangeOID:        Int4range,
+		pgtype.NumrangeOID:         Numrange,
+		pgtype.TsrangeOID:          Tsrange,
+		pgtype.TstzrangeOID:        Tstzrange,
+		pgtype.DaterangeOID:        Daterange,
+		pgtype.Int8rangeOID:        Int8range,
 	}
 )
 
-func FetchOIDTypes(conn *pgx.Conn, oids ...OID) (map[OID]Type, error) {
-	types := make(map[OID]Type, len(oids))
-	oidsToFetch := make([]OID, 0, len(oids))
+func FetchOIDTypes(conn *pgx.Conn, oids ...OIDInt) (map[OIDInt]Type, error) {
+	types := make(map[OIDInt]Type, len(oids))
+	oidsToFetch := make([]OIDInt, 0, len(oids))
 	typeMapLock.Lock()
 	for _, oid := range oids {
 		if t, ok := typeMap[oid]; ok {
