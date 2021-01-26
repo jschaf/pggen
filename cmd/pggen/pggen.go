e002ce5a08812ba6e42a5f7a4e2e4022348dafb4
Joe Schafer <joe@jschaf.com>
2021-01-26 13:20:18 -0800

    Add nop pggen binary

--- cmd/pggen/pggen.go
+++ cmd/pggen/pggen.go
@@ -1,4 +1,69 @@
-package pggen
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"github.com/peterbourgon/ff/v3/ffcli"
+	"os"
+)
+
+const flagHelp = `
+pggen generates type-safe code from a files containing Postgres queries.
+`
+
+func run() error {
+	genCmd := newGenCmd()
+	rootFlagSet := flag.NewFlagSet("root", flag.ExitOnError)
+	rootCmd := &ffcli.Command{
+		ShortUsage: "pggen <subcommand> [options...]",
+		LongHelp:   flagHelp[1 : len(flagHelp)-1], // remove lead/trail newlines
+		FlagSet:    rootFlagSet,
+		Subcommands: []*ffcli.Command{
+			genCmd,
+		},
+	}
+	rootCmd.Exec = func(ctx context.Context, args []string) error {
+		fmt.Println(ffcli.DefaultUsageFunc(rootCmd))
+		os.Exit(1)
+		return nil
+	}
+	if err := rootCmd.ParseAndRun(context.Background(), os.Args[1:]); err != nil {
+		return fmt.Errorf("run pggen cmd: %w", err)
+	}
+	return nil
+}
+
+func newGenCmd() *ffcli.Command {
+	fset := flag.NewFlagSet("go", flag.ExitOnError)
+	goSubCmd := &ffcli.Command{
+		Name:       "go",
+		ShortUsage: "pggen gen go [options...]",
+		ShortHelp:  "generates go code for Postgres query files",
+		FlagSet:    fset,
+		Exec: func(ctx context.Context, args []string) error {
+			fmt.Println("gen go")
+			return nil
+		},
+	}
+	cmd := &ffcli.Command{
+		Name:        "gen",
+		ShortUsage:  "pggen gen (go|<lang>) [options...]",
+		ShortHelp:   "generates code in specific language for Postgres query files",
+		FlagSet:     nil,
+		Subcommands: []*ffcli.Command{goSubCmd},
+	}
+	cmd.Exec = func(ctx context.Context, args []string) error {
+		fmt.Println(ffcli.DefaultUsageFunc(cmd))
+		os.Exit(1)
+		return nil
+	}
+	return cmd
+}
 
 func main() {
+	if err := run(); err != nil {
+		fmt.Printf("ERROR: %s\n", err.Error())
+		os.Exit(1)
+	}
 }
--- go.mod
+++ go.mod
@@ -4,11 +4,11 @@ go 1.15
 
 require (
 	github.com/google/go-cmp v0.5.4
-	github.com/iancoleman/strcase v0.1.3
 	github.com/jackc/pgconn v1.8.0
 	github.com/jackc/pgproto3/v2 v2.0.6
 	github.com/jackc/pgtype v1.6.2
 	github.com/jackc/pgx/v4 v4.10.1
+	github.com/peterbourgon/ff/v3 v3.0.0
 	github.com/rakyll/statik v0.1.7
 	github.com/stretchr/testify v1.5.1
 	go.uber.org/multierr v1.5.0
