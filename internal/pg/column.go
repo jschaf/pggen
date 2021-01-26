43f9e711bdbd83c9752d6bbbc2e2675f27038ae9
Joe Schafer <joe@jschaf.com>
2021-01-25 23:29:15 -0800

    Add pg.FetchColumns to get meta info about table columns

---
+++ internal/pg/column.go
@@ -0,0 +1,94 @@
+package pg
+
+import (
+	"context"
+	"fmt"
+	"github.com/jackc/pgx/v4"
+	"github.com/jschaf/pggen/internal/texts"
+	"strconv"
+	"strings"
+	"time"
+)
+
+// Column stores information about a column in a TableOID.
+// https://www.postgresql.org/docs/13/catalog-pg-attribute.html
+type Column struct {
+	Name      string // pg_attribute.attname: column name
+	TableOID  OIDInt // pg_attribute:attrelid: TableOID the column belongs to
+	TableName string // pg_class.relname: name of TableOID that owns the column
+	Order     int    // pg_attribute.attnum: the number of column starting from 1
+	Type      Type   // pg_attribute.atttypid: data type of the column
+	Null      bool   // pg_attribute.attnotnull: represents a not-null constraint
+}
+
+// ColumnKey is a composite key of a TableOID OID and a number of the column
+// within the TableOID.
+type ColumnKey struct {
+	TableOID OIDInt
+	Num      int
+}
+
+var columnsCache = make(map[ColumnKey]Column, 32)
+
+// FetchColumns fetches meta information about a Postgres column from the
+// pg_class and pg_attribute catalog tables.
+func FetchColumns(conn *pgx.Conn, keys []ColumnKey) ([]Column, error) {
+	if len(keys) == 0 {
+		return nil, nil
+	}
+
+	// Build query predicate.
+	predicate := &strings.Builder{}
+	predicate.Grow(len(keys) * 15)
+	for i, key := range keys {
+		predicate.WriteString("(cls.oid = ")
+		predicate.WriteString(strconv.Itoa(int(key.TableOID)))
+		predicate.WriteString(" AND attr.attnum = ")
+		predicate.WriteString(strconv.Itoa(key.Num))
+		predicate.WriteString(")")
+		if i < len(keys)-1 {
+			predicate.WriteString("\n    OR ")
+		}
+	}
+
+	// Execute query.
+	q := texts.Dedent(`
+		SELECT cls.oid         AS table_oid,
+					 cls.relname     AS table_name,
+					 attr.attname    AS col_name,
+					 attr.attnum     AS col_num,
+					 attr.attnotnull AS col_null
+		FROM pg_class cls
+					 JOIN pg_attribute attr ON (attr.attrelid = cls.oid)
+	`) + "\n    WHERE " + predicate.String()
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	rows, err := conn.Query(ctx, q)
+	if err != nil {
+		return nil, fmt.Errorf("fetch column metadata: %w", err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		col := Column{}
+		notNull := false
+		if err := rows.Scan(&col.TableOID, &col.TableName, &col.Name, &col.Order, &notNull); err != nil {
+			return nil, fmt.Errorf("scan fetch column row: %w", err)
+		}
+		col.Null = !notNull
+		columnsCache[ColumnKey{col.TableOID, col.Order}] = col
+	}
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("close fetch column rows: %w", err)
+	}
+
+	// Return the columns in requested order.
+	cols := make([]Column, 0, len(keys))
+	for _, key := range keys {
+		col, ok := columnsCache[key]
+		if !ok {
+			return nil, fmt.Errorf("missing column after fetch table_oid=%d Num=%d", key.TableOID, key.Num)
+		}
+		cols = append(cols, col)
+	}
+	return cols, nil
+}
---
+++ internal/pg/column_test.go
@@ -0,0 +1,84 @@
+package pg
+
+import (
+	"context"
+	"github.com/google/go-cmp/cmp"
+	"github.com/jackc/pgx/v4"
+	"github.com/jschaf/pggen/internal/pgtest"
+	"github.com/jschaf/pggen/internal/texts"
+	"testing"
+	"time"
+)
+
+func TestFetchColumns(t *testing.T) {
+	tests := []struct {
+		name    string
+		schema  string
+		colNums []int
+		want    []Column
+	}{
+		{"empty", "", nil, nil},
+		{
+			"one col null",
+			"CREATE TABLE author ( first_name text );",
+			[]int{1},
+			[]Column{{Name: "first_name", TableName: "author", Order: 1, Null: true}},
+		},
+		{
+			"one col not null",
+			"CREATE TABLE author ( first_name text NOT NULL);",
+			[]int{1},
+			[]Column{{Name: "first_name", TableName: "author", Order: 1, Null: false}},
+		},
+		{
+			"two col mixed",
+			"CREATE TABLE author ( first_name text NOT NULL, last_name text);",
+			[]int{2, 1},
+			[]Column{
+				{Name: "last_name", TableName: "author", Order: 2, Null: true},
+				{Name: "first_name", TableName: "author", Order: 1, Null: false},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			conn, cleanup := pgtest.NewPostgresSchemaString(t, tt.schema)
+			defer cleanup()
+			oid := findTableOID(t, conn, "author")
+			keys := make([]ColumnKey, len(tt.colNums))
+			for i, num := range tt.colNums {
+				keys[i] = ColumnKey{oid, num}
+			}
+			cols, err := FetchColumns(conn, keys)
+			if err != nil {
+				t.Fatal(err)
+			}
+			// Add table OID to each key.
+			for i, col := range tt.want {
+				col.TableOID = oid
+				tt.want[i] = col
+			}
+			if diff := cmp.Diff(tt.want, cols); diff != "" {
+				t.Errorf("FetchColumns() query mismatch (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
+
+func findTableOID(t *testing.T, conn *pgx.Conn, table string) OIDInt {
+	sql := texts.Dedent(`
+		SELECT oid AS table_oid
+		FROM pg_class
+		WHERE relname = $1
+		ORDER BY table_oid DESC
+		LIMIT 1;
+	`)
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	row := conn.QueryRow(ctx, sql, table)
+	var oid OIDInt = 0
+	if err := row.Scan(&oid); err != nil {
+		t.Fatal(err)
+	}
+	return oid
+}
