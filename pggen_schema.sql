3585317de1b2637286ffffe720ab9e4d67a90c2e
Joe Schafer <joe@jschaf.com>
2021-01-27 11:39:50 -0800

    Add docs for pggen.arg

--- pggen_schema.sql
+++ pggen_schema.sql
@@ -1,6 +1,8 @@
--- This schema file exists solely so IntelliJ doesn't mark every pggen.arg()
--- in squiggly red.
+-- This schema file exists solely so IntelliJ doesn't underline every
+-- pggen.arg() expression in squiggly red.
 CREATE SCHEMA pggen;
 
-CREATE FUNCTION pggen.arg(arg any) RETURNS any AS
+-- pggen.arg defines a named parameter that's eventually compiled into a
+-- placeholder for a prepared query: $1, $2, etc.
+CREATE FUNCTION pggen.arg(param text) RETURNS any AS
 '';
