182f48d9fc57fb764bdec9ff0926e602db449b55
Joe Schafer <joe@jschaf.com>
2021-01-28 22:42:32 -0800

    Standardize example format on codegen_test.go and query.sql

--- example/author/codegen_test.go
+++ example/author/codegen_test.go
@@ -1,6 +1,7 @@
-package codegen
+package author
 
 import (
+	"github.com/jschaf/pggen/codegen"
 	"github.com/jschaf/pggen/codegen/gen"
 	"github.com/jschaf/pggen/internal/pgtest"
 	"github.com/stretchr/testify/assert"
@@ -10,37 +11,33 @@ import (
 )
 
 func TestGenerate_Go_Example_Author(t *testing.T) {
-	conn, cleanupFunc := pgtest.NewPostgresSchema(t, []string{
-		"../example/author/schema.sql",
-	})
+	conn, cleanupFunc := pgtest.NewPostgresSchema(t, []string{"schema.sql"})
 	defer cleanupFunc()
 
 	tmpDir := t.TempDir()
-	err := Generate(
+	err := codegen.Generate(
 		gen.GenerateOptions{
 			ConnString: conn.Config().ConnString(),
-			QueryFiles: []string{
-				"../example/author/queries.sql",
-			},
-			OutputDir: tmpDir,
-			GoPackage: "author",
-			Language:  gen.LangGo,
+			QueryFiles: []string{"query.sql"},
+			OutputDir:  tmpDir,
+			GoPackage:  "author",
+			Language:   gen.LangGo,
 		})
 	if err != nil {
 		t.Fatalf("Generate() example/author: %s", err)
 	}
 
-	wantQueriesFile := "../example/author/queries.sql.go"
-	gotQueriesFile := filepath.Join(tmpDir, "queries.sql.go")
+	wantQueriesFile := "query.sql.go"
+	gotQueriesFile := filepath.Join(tmpDir, "query.sql.go")
 	assert.FileExists(t, gotQueriesFile,
-		"Generate() should emit queries.sql.go")
+		"Generate() should emit query.sql.go")
 	wantQueries, err := ioutil.ReadFile(wantQueriesFile)
 	if err != nil {
-		t.Fatalf("read wanted queries.go.sql: %s", err)
+		t.Fatalf("read wanted query.go.sql: %s", err)
 	}
 	gotQueries, err := ioutil.ReadFile(gotQueriesFile)
 	if err != nil {
-		t.Fatalf("read generated queries.go.sql: %s", err)
+		t.Fatalf("read generated query.go.sql: %s", err)
 	}
 	assert.Equalf(t, string(wantQueries), string(gotQueries),
 		"Got file %s; does not match contents of %s",
