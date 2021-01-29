c06a97f16282b3265fa323690c40c6dc63565878
Joe Schafer <joe@jschaf.com>
2021-01-28 17:37:47 -0800

    Add batch query tests

--- example/author/queries.sql_test.go
+++ example/author/queries.sql_test.go
@@ -42,6 +42,28 @@ func TestNewQuerier_FindAuthorByID(t *testing.T) {
 	}
 }
 
+func TestNewQuerier_FindAuthorByID_Batch(t *testing.T) {
+	conn, cleanup := pgtest.NewPostgresSchema(t, []string{"schema.sql"})
+	defer cleanup()
+	q := NewQuerier(conn)
+	adamsID := insertAuthor(t, q, "john", "adams")
+	insertAuthor(t, q, "george", "washington")
+
+	batch := &pgx.Batch{}
+	q.FindAuthorByIDBatch(context.Background(), batch, adamsID)
+	results := conn.SendBatch(context.Background(), batch)
+	authors, err := q.FindAuthorByIDScan(results)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, FindAuthorByIDRow{
+		AuthorID:  adamsID,
+		FirstName: "john",
+		LastName:  "adams",
+		Suffix:    pgtype.Text{Status: pgtype.Null},
+	}, authors, "authorByID should match")
+}
+
 func TestNewQuerier_FindAuthors(t *testing.T) {
 	conn, cleanup := pgtest.NewPostgresSchema(t, []string{"schema.sql"})
 	defer cleanup()
@@ -78,6 +100,29 @@ func TestNewQuerier_FindAuthors(t *testing.T) {
 			assert.Equal(t, tt.want, authors, "expect authors to match expected")
 		})
 	}
+}
+
+func TestNewQuerier_FindAuthors_Batch(t *testing.T) {
+	conn, cleanup := pgtest.NewPostgresSchema(t, []string{"schema.sql"})
+	defer cleanup()
+	q := NewQuerier(conn)
+	insertAuthor(t, q, "john", "adams")
+	washingtonID := insertAuthor(t, q, "george", "washington")
+	carverID := insertAuthor(t, q, "george", "carver")
+
+	batch := &pgx.Batch{}
+	q.FindAuthorsBatch(context.Background(), batch, "george")
+	results := conn.SendBatch(context.Background(), batch)
+	authors, err := q.FindAuthorsScan(results)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := []FindAuthorsRow{
+		{AuthorID: washingtonID, FirstName: "george", LastName: "washington", Suffix: pgtype.Text{Status: pgtype.Null}},
+		{AuthorID: carverID, FirstName: "george", LastName: "carver", Suffix: pgtype.Text{Status: pgtype.Null}},
+	}
+	assert.Equal(t, want, authors, "authorByID should match")
 }
 
 func TestNewQuerier_InsertAuthorSuffix(t *testing.T) {
@@ -119,6 +164,32 @@ func TestNewQuerier_DeleteAuthorsByFirstName(t *testing.T) {
 		t.Fatal(err)
 	}
 	assert.Empty(t, authors, "no authors should remain with first name of george")
+}
+
+func TestNewQuerier_DeleteAuthorsByFirstName_Batch(t *testing.T) {
+	conn, cleanup := pgtest.NewPostgresSchema(t, []string{"schema.sql"})
+	defer cleanup()
+	q := NewQuerier(conn)
+	insertAuthor(t, q, "john", "adams")
+	insertAuthor(t, q, "george", "washington")
+	insertAuthor(t, q, "george", "carver")
+
+	batch := &pgx.Batch{}
+	q.DeleteAuthorsByFirstNameBatch(context.Background(), batch, "george")
+	results := conn.SendBatch(context.Background(), batch)
+	_, err := q.DeleteAuthorsByFirstNameScan(results)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := results.Close(); err != nil {
+		t.Fatal(err)
+	}
+
+	georges, err := q.FindAuthors(context.Background(), "george")
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Empty(t, georges, "should be no georges")
 }
 
 func TestNewQuerier_DeleteAuthorsByFullName(t *testing.T) {
