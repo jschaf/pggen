195bc0953999f796c08ce94970904cb622a1d7ab
Joe Schafer <joe@jschaf.com>
2021-02-03 11:43:02 -0800

    Add diff check to go integration test

--- example/integration_test.go
+++ example/integration_test.go
@@ -1,15 +1,19 @@
 package example
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"sync"
 	"testing"
 )
 
 var update = flag.Bool("update", false, "update integration tests if true")
+
+const projDir = ".." // hardcoded
 
 func TestMain(m *testing.M) {
 	flag.Parse()
@@ -26,18 +30,19 @@ func TestExamples(t *testing.T) {
 		"--schema-glob", "example/enums/schema.sql",
 		"--query-glob", "example/enums/query.sql",
 	)
+	assertNoDiff(t)
 }
 
 func runPggen(t *testing.T, pggen string, args ...string) string {
 	cmd := exec.Cmd{
 		Path: pggen,
 		Args: append([]string{pggen, "gen", "go"}, args...),
-		Dir:  "..",
+		Dir:  projDir,
 	}
 	t.Log("running pggen")
 	output, err := cmd.CombinedOutput()
 	if err != nil {
-		t.Log(string(output))
+		t.Log("pggen output:\n" + string(bytes.TrimSpace(output)))
 		t.Fatalf("run pggen: %s", err)
 	}
 	return pggen
@@ -54,13 +59,53 @@ func compilePggen(t *testing.T) string {
 		Path: goBin,
 		Args: []string{goBin, "build", "-o", pggen, "./cmd/pggen"},
 		Env:  os.Environ(),
-		Dir:  "..",
+		Dir:  projDir,
 	}
 	t.Log("compiling pggen")
 	output, err := cmd.CombinedOutput()
 	if err != nil {
-		t.Log(string(output))
+		t.Log("go build output:\n" + string(bytes.TrimSpace(output)))
 		t.Fatalf("compile pggen: %s", err)
 	}
 	return pggen
+}
+
+var (
+	gitBin     string
+	gitBinErr  error
+	gitBinOnce = &sync.Once{}
+)
+
+func assertNoDiff(t *testing.T) {
+	gitBinOnce.Do(func() {
+		gitBin, gitBinErr = exec.LookPath("git")
+		if gitBinErr != nil {
+			gitBinErr = fmt.Errorf("lookup git path: %w", gitBinErr)
+		}
+	})
+	if gitBinErr != nil {
+		t.Fatal(gitBinErr)
+	}
+	updateIndexCmd := exec.Cmd{
+		Path: gitBin,
+		Args: []string{gitBin, "update-index", "--refresh"},
+		Env:  os.Environ(),
+		Dir:  projDir,
+	}
+	updateOutput, err := updateIndexCmd.CombinedOutput()
+	if err != nil {
+		t.Log("git update-index output:\n" + string(bytes.TrimSpace(updateOutput)))
+		t.Fatalf("git update-index: %s", err)
+	}
+	diffIndexCmd := exec.Cmd{
+		Path: gitBin,
+		Args: []string{gitBin, "diff-index", "--quiet", "HEAD", "--"},
+		Env:  os.Environ(),
+		Dir:  projDir,
+	}
+	diffOutput, err := diffIndexCmd.CombinedOutput()
+	if err != nil {
+		t.Log("git diff-index output:\n" + string(bytes.TrimSpace(diffOutput)))
+		t.Fatalf("git diff-index: %s", err)
+	}
 }
