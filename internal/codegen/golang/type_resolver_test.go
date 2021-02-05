ccdd5e1189abfebe9ce5732ab474c4a0e50c65c3
Joe Schafer <joe@jschaf.com>
2021-02-05 01:09:25 -0800

    Merge types.go and type_resolver.go

--- internal/codegen/golang/type_resolver_test.go
+++ internal/codegen/golang/type_resolver_test.go
@@ -65,3 +65,85 @@ func TestTypeResolver_Resolve(t *testing.T) {
 		})
 	}
 }
+
+func TestType_QualifyRel(t *testing.T) {
+	caser := casing.NewCaser()
+	tests := []struct {
+		typ          gotype.Type
+		otherPkgPath string
+		want         string
+	}{
+		{
+			typ: gotype.NewEnumType(
+				"example.com/foo",
+				pg.EnumType{Name: "device", Labels: []string{"macos"}},
+				caser,
+			),
+			otherPkgPath: "example.com/bar",
+			want:         "foo.Device",
+		},
+		{
+			typ: gotype.NewEnumType(
+				"example.com/bar",
+				pg.EnumType{Name: "device", Labels: []string{"macos"}},
+				caser,
+			),
+			otherPkgPath: "example.com/bar",
+			want:         "Device",
+		},
+		{
+			typ:          gotype.NewOpaqueType("example.com/bar.Baz"),
+			otherPkgPath: "example.com/bar",
+			want:         "Baz",
+		},
+		{
+			typ:          gotype.NewOpaqueType("string"),
+			otherPkgPath: "example.com/bar",
+			want:         "string",
+		},
+		{
+			typ:          gotype.NewOpaqueType("string"),
+			otherPkgPath: "",
+			want:         "string",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.typ.Import()+"."+tt.typ.BaseName(), func(t *testing.T) {
+			got := tt.typ.QualifyRel(tt.otherPkgPath)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func TestCreateCompositeType(t *testing.T) {
+	caser := casing.NewCaser()
+	resolver := NewTypeResolver(caser, nil)
+	tests := []struct {
+		pkgPath string
+		pgType  pg.CompositeType
+		want    gotype.CompositeType
+	}{
+		{
+			pkgPath: "example.com/foo",
+			pgType: pg.CompositeType{
+				Name:        "qux",
+				ColumnNames: []string{"one", "two_a"},
+				ColumnTypes: []pg.Type{pg.Text, pg.Int8},
+			},
+			want: gotype.CompositeType{
+				PkgPath:    "example.com/foo",
+				Pkg:        "foo",
+				Name:       "Qux",
+				FieldNames: []string{"One", "TwoA"},
+				FieldTypes: []gotype.Type{gotype.PgText, gotype.PgInt8},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.pkgPath+" - "+tt.pgType.Name, func(t *testing.T) {
+			got, err := CreateCompositeType(tt.pkgPath, tt.pgType, resolver, caser)
+			assert.NoError(t, err)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
