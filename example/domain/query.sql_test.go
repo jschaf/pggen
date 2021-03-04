7dc0e0b611429c22bdf78b94e40a1fac7536bf27
Joe Schafer <joe@jschaf.com>
2021-03-03 23:04:30 -0800

    Standardize testing format for example/domain

--- example/domain/query.sql_test.go
+++ example/domain/query.sql_test.go
@@ -2,21 +2,34 @@ package domain
 
 import (
 	"context"
+	"github.com/jackc/pgx/v4"
+	"github.com/jschaf/pggen/internal/errs"
 	"github.com/jschaf/pggen/internal/pgtest"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
 
-func TestQuerier(t *testing.T) {
+func TestQuerier_DomainOne(t *testing.T) {
 	conn, cleanup := pgtest.NewPostgresSchema(t, []string{"schema.sql"})
 	defer cleanup()
 
 	q := NewQuerier(conn)
 	ctx := context.Background()
 
-	postCode, err := q.DomainOne(ctx)
-	if err != nil {
-		t.Fatal(err)
-	}
-	assert.Equal(t, "90210", postCode)
+	t.Run("DomainOne", func(t *testing.T) {
+		postCode, err := q.DomainOne(ctx)
+		require.NoError(t, err)
+		assert.Equal(t, "90210", postCode)
+	})
+
+	t.Run("DomainOneBatch", func(t *testing.T) {
+		batch := &pgx.Batch{}
+		q.DomainOneBatch(batch)
+		results := conn.SendBatch(context.Background(), batch)
+		got, err := q.DomainOneScan(results)
+		defer errs.CaptureT(t, results.Close, "close batch")
+		require.NoError(t, err)
+		assert.Equal(t, "90210", got)
+	})
 }
